bc5c5114c01e1eb9e2eb066d633dc89a59a68d50
ShardulVanage <vsharadul@gmail.com>
2024-02-15 23:24:23 +0530

    s

--- my-app/src/components/Pricing.jsx
+++ my-app/src/components/Pricing.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Pricing() {
   return (
     <section id='price' className='py-24 flex flex-col justify-center items-center bg-gradient-to-r from-rose-400/90 via-fuchsia-500/60 to-indigo-500/60'>
-        <div className=' border-double border-8 bg-white px-4 h-full rounded-3xl  w-screen '>
+        <div className=' border-double border-8 bg-white px-4 h-full rounded-3xl sm:w-5/12  w-screen '>
              <h1 className='text-3xl text-center tracking-wide font-extrabold py-4 pt-8'>Data Master Placement Guarantee <br /> Course</h1>
              <p className='text-2xl text-center'>( Get More Than 80% Discount Today )</p>
-             <div className='flex flex-row justify-center items-baseline py-2 pl-12' >
+             <div className='flex flex-row justify-center items-baseline py-2 pl-12 mr-4' >
                 <h1 className='text-xl pr-1 font-medium '><del>50,000₹</del></h1>
                 <h1 className='text-5xl font-bold bg-clip-text text-transparent  bg-gradient-to-r from-rose-400/80 via-fuchsia-500/80 to-indigo-500/80 drop-shadow-md'>4,999₹ </h1>
                  <h1 className='text-4xl pr-1 font-bold pl-2 bg-clip-text text-transparent  bg-gradient-to-r from-rose-400/80 via-fuchsia-500/80 to-indigo-500/80 drop-shadow-md'><span className='text-black'>(</span>70$<span className='text-black'>)</span></h1>
