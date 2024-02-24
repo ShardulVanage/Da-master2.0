77c9f68551227a757ec0fa7eb4c34e96cb1515a3
ShardulVanage <vsharadul@gmail.com>
2024-02-24 14:48:09 +0530

    syllabus pdf added

--- my-app/src/components/Brocher.jsx
+++ my-app/src/components/Brocher.jsx
@@ -7,7 +7,7 @@ function Brocher() {
         <h1 className='py-4 sm:text-6xl text-3xl font-semibold '>
             What Are You Getting?
         </h1>
-        <a className=' text-sm sm:text-xl bg-gradient-to-r from-gray-100/20 to-gray-300/30  my-2  py-7 px-12 sm:px-24 rounded-2xl font-medium  border-double border-8' href=" ">
+        <a href="/syllabus.pdf" className=' text-sm sm:text-xl bg-gradient-to-r from-gray-100/20 to-gray-300/30  my-2  py-7 px-12 sm:px-24 rounded-2xl font-medium  border-double border-8'>
            Download Syllabus
         </a>
         
